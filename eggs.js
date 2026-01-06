d69b930460d9d60028f770b907446a9c48100a58
Vivian Rose <26861054+Roachbones@users.noreply.github.com>
2026-01-06 07:27:14 -0600

    caterpiller

--- eggs.js
+++ eggs.js
@@ -95,6 +95,11 @@ function invalid_guess_egg_message(guess) {
         return "Not quite how it's spelled.";
     }
     if (guess=='dear' && !guesses.includes('deer')) { return "Wrong spelling, dear."; }
+    if (guess=='caterpiller') {
+        // TODO review this one
+        queue_trivium("“caterpillar” is spelled with “-pillar”, not “-piller”, but the etymology derives from the Middle English «<a href=https://en.wiktionary.org/wiki/piller#Etymology_1>piller</a>», meaning to plunder. Presumably because they eat so much.");
+        return "It ends in -pillar, not -piller.";
+    }
     if (guess=='cryptobug') { return "That's a brand name."; }
     if (guess=='mushroom') { return "No, fungi aren't animals."; }
     if (guess=='cheeta') { return "You're missing a letter."; }
